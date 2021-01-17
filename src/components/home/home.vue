19b72e0d848c3dc9d2c776ee9e0169ba3f8c20d5
v.duguanyu <v.duguanyu@yuewen.com>
2021-01-17 18:36:12 +0800

    modified home.vue

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
-    <div class="logo">Fast Connect DB</div>
     <div class="op-box">
-        This is a new page
+        This is a new page, why you can't commit?
     </div>
     <div class="res-box"></div>
   </div>
